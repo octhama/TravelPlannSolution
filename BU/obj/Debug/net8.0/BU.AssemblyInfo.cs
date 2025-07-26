c0cd84ca9083addefa0de150f78b63b59dcc9e65
Hasler TEHOU <47756458+octhama@users.noreply.github.com>
2025-07-26 14:49:50 +0200

    Update build outputs and resource stamps
    Regenerated build artifacts for BU, Common, DAL, and TravelPlannMauiApp projects. Added and updated resource stamp files and linked assemblies for MacCatalyst, and removed obsolete resource stamps to reflect current dependencies.

--- BU/obj/Debug/net8.0/BU.AssemblyInfo.cs
+++ BU/obj/Debug/net8.0/BU.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BU")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce093e252b09ec4c873dfc7c9a94f76bc40bb063")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c1728d358f9de277a2e6ff93229d7090925d8df")]
 [assembly: System.Reflection.AssemblyProductAttribute("BU")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BU")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
