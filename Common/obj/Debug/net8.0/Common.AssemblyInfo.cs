30e7a201e8606ed58cc0eef3e4e4fe073ae393b0
Hasler TEHOU <47756458+octhama@users.noreply.github.com>
2025-07-29 22:41:42 +0200

    Update build outputs and resource stamps
    Regenerated build artifacts for BU, Common, DAL, and TravelPlannMauiApp projects. Added and updated resource stamp files for TravelPlannMauiApp to reflect new dependencies and build state.

--- Common/obj/Debug/net8.0/Common.AssemblyInfo.cs
+++ Common/obj/Debug/net8.0/Common.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Common")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de36817dfec4aba9c6390e9491763749ba64315a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9df96d3d407e0df2fa966dfc23ebb499e9b9aa46")]
 [assembly: System.Reflection.AssemblyProductAttribute("Common")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Common")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- DAL/DB/TravelPlannDbContext.cs
+++ DAL/DB/TravelPlannDbContext.cs
@@ -35,9 +35,9 @@ public partial class TravelPlannDbContext : DbContext
 
     public virtual DbSet<Voyage> Voyages { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    /**protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=localhost,1433;Database=TravelPlanner;User Id=sa;Password=1235OHdf%e;TrustServerCertificate=True;");
+        => optionsBuilder.UseSqlServer("Server=localhost,1433;Database=TravelPlanner;User Id=sa;Password=1235OHdf%e;TrustServerCertificate=True;");**/
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
